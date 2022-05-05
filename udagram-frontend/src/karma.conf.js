9c87f0ff333de819b97d1f55e52d9ba6c91726b7
muhammadalsattar <muhammadalsattar@gmail.com>
2022-05-05 17:42:04 +0200

    initial commit

--- udagram-frontend/src/karma.conf.js
+++ udagram-frontend/src/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['CircleCI_ChromeHeadless'],
+    browsers: ['CircleCI_Chrome'],
     singleRun: false
   });
 };
