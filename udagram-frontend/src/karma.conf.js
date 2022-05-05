1331bce2d20577bf55cd4f32fc0fd2df3443a2b6
muhammadalsattar <muhammadalsattar@gmail.com>
2022-05-05 20:04:56 +0200

    initial commit

--- udagram-frontend/src/karma.conf.js
+++ udagram-frontend/src/karma.conf.js
@@ -25,20 +25,20 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    // customLaunchers: {
-    //   CircleCI_ChromeHeadless: {
-    //     base: 'ChromeHeadless',
-    //     flags: [
-    //       '--headless',
-    //       '--disable-gpu',
-    //       '--disable-translate',
-    //       '--disable-extensions',
-    //       '--no-sandbox',
-    //      '--remote-debugging-port=9222',
-    //     ],
-    //   }
-    // },
+    browsers: ['CircleCI_ChromeHeadless'],
+    customLaunchers: {
+      CircleCI_ChromeHeadless: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+          '--disable-translate',
+          '--disable-extensions',
+          '--no-sandbox',
+         '--remote-debugging-port=9222',
+        ],
+      }
+    },
     singleRun: false
   });
 };
