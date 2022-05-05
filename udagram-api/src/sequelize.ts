73738feb39909f003953009ae8e946e266549d6c
muhammadalsattar <muhammadalsattar@gmail.com>
2022-05-05 15:32:55 +0200

    initial commit

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,4 +1,4 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}`);
+export const sequelize = new Sequelize('postgres://'+config.username+':'+config.password+'@'+config.host+':'+config.port+'/'+config.database);
